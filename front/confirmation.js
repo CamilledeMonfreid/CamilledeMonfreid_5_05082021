a9d4462dcf1c04e7e7c3fb28329c8a6075dd82d9
Camille de Monfreid <camille.de-monfreid@outlook.fr>
2021-09-28 11:24:52 +0200

    Responsive

--- front/confirmation.js
+++ front/confirmation.js
@@ -1,26 +1,36 @@
 let produitStorage = JSON.parse(localStorage.getItem("produit"))
 let products=[];
+if(produitStorage){
+    for(let i = 0; i<produitStorage.length;i++){
+        products.push(produitStorage[i].idProduit)
+    }
+
+}
+
+let pageConfirmation = document.getElementById("confirm")
+let erreurConfirmation = document.getElementById("erreurConfirmation")
 
 
 
 if(products.length ==0){
-    let pageConfirmation = document.getElementById("confirm")
+    erreurConfirmation.style.display="flex"
     pageConfirmation.style.display="none"
+    console.log(products)
     
 } else{
+    pageConfirmation.style.display="flex"
+    erreurConfirmation.style.display="none"
     let contactStorage = JSON.parse(localStorage.getItem("contact"))
     let contact= contactStorage[0]
 
     let totalPrice = JSON.parse(localStorage.getItem("totalPrice"))
     let afficherTotalPrice = document.createElement("p")
-    afficherTotalPrice.textContent = "Pour rappel, le prix total de votre commande est de" + totalPrice +"€"
+    afficherTotalPrice.textContent = "Pour rappel, le prix total de votre commande est de " + totalPrice +"€"
     let essai = document.getElementById("confirm")
     console.log(essai)
     essai.appendChild(afficherTotalPrice)
 
-    for(let i = 0; i<produitStorage.length;i++){
-        products.push(produitStorage[i].idProduit)
-    }
+    
 
     const aEnvoyer = {
         products,
